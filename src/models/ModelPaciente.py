79d78bab104165eef3848197e170990e5f4ebb52
Víctor Ponce <vic.manuel35@gmail.com>
2023-05-07 21:30:16 -0600

    ajustes

--- src/models/ModelPaciente.py
+++ src/models/ModelPaciente.py
@@ -2,11 +2,11 @@ from.entities.Paciente import Paciente
 
 class ModelPaciente():
     @classmethod # CAMBIAR PARA QUE FUNCIONEEEEEE
-    def register(self,db,user): 
+    def registerPaciente(self,db,paciente): 
         #haz el mismo metodo de arriba, pero sin pedir el tipo de usuario, ademas pide el nombre, apellido paterno y materno, sexo, telefono y su correo y contraseña, usando el modelo de UserRegistro
         try:
             cursor = db.connection.cursor()
-            sql=""" INSERT INTO paciente (nombre, apellidoPaterno, apellidoMaterno, sexo, telefono, correoElectronico, contraseña) VALUES ('{}','{}','{}','{}','{}','{}','{}') """.format(user.nombre,user.apellidoPaterno,user.apellidoMaterno,user.sexo,user.telefono,user.correoElectronico,user.contraseña)
+            sql=""" INSERT INTO infousuarios (idUsuario,nombre, apellidoPaterno, apellidoMaterno,fechaNacimiento, sexo, telefono) VALUES ('{}','{}','{}','{}','{}','{}','{}') """.format(paciente.idUsuario,paciente.nombre,paciente.apellidoPaterno,paciente.apellidoMaterno,paciente.fechaNacimiento,paciente.sexo,paciente.telefono)
             cursor.execute(sql)
             db.connection.commit()
             print("Usuario registrado")
@@ -14,3 +14,5 @@ class ModelPaciente():
         except Exception as ex:
             print("No se pudo registrar")
             raise Exception(ex)
+
+        
--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,6 +1,41 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 #si quiero probar con una contraseña nueva importar funcion ", generate_password_hash"
+
+#modificaciones a user desde aqui ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 class User():
+    def __init__(self, idUsuario,tipoUsuario, correoElectronico, contraseña ):
+        self.idUsuario = idUsuario
+        self.tipoUsuario = tipoUsuario
+        self.correoElectronico = correoElectronico
+        self.contraseña = contraseña
+
+    @classmethod #para no instanciar la clase  
+    def check_password(self, hashed_password, contraseña):
+        return check_password_hash(hashed_password, contraseña)
+    
+    
+    
+class UserRegistro():
+    def __init__(self, idUsuario,tipoUsuario, correoElectronico, contraseña ):
+        self.idUsuario = idUsuario
+        self.tipoUsuario = tipoUsuario
+        self.correoElectronico = correoElectronico
+        self.contraseña = contraseña
+    
+    @classmethod
+    def hash_password(self, contraseña):
+        return generate_password_hash(contraseña)
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+""" class User():
     def __init__(self, numPaciente, correoElectronico, contraseña, tipoUsuario):
         self.numPaciente = numPaciente
         self.correoElectronico = correoElectronico
@@ -34,40 +69,9 @@ class UserRegistro():
     
     @classmethod
     def hash_password(self, contraseña):
-        return generate_password_hash(contraseña)
+        return generate_password_hash(contraseña) """
     
 #print(generate_password_hash('bigmac'))
 
 
 
-#modificaciones a user desde aqui ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-class User():
-    def __init__(self, idUsuario,tipoUsuario, correoElectronico, contraseña ):
-        self.idUsuario = idUsuario
-        self.tipoUsuario = tipoUsuario
-        self.correoElectronico = correoElectronico
-        self.contraseña = contraseña
-
-    @classmethod #para no instanciar la clase  
-    def check_password(self, hashed_password, contraseña):
-        return check_password_hash(hashed_password, contraseña)
-    
-    
-    
-class UserRegistro():
-    def __init__(self, idUsuario,tipoUsuario, correoElectronico, contraseña ):
-        self.idUsuario = idUsuario
-        self.tipoUsuario = tipoUsuario
-        self.correoElectronico = correoElectronico
-        self.contraseña = contraseña
-    
-    @classmethod
-    def hash_password(self, contraseña):
-        return generate_password_hash(contraseña)
-
-
-
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
