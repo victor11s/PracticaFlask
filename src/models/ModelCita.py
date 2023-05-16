9a8585751061dca099996ba65540515f2a73949a
victor11s <vic.manuel35@gmail.com>
2023-05-15 22:13:29 -0600

    ajustes

--- src/models/ModelCita.py
+++ src/models/ModelCita.py
@@ -1,16 +1,44 @@
 from.entities.Cita import Cita
 
 class ModelCita():
+    #Inserta una cita en la base de datos
     @classmethod
-    def crearCita(self,db,cita):
+    def crearCita(cls, db, cita):
         try:
             cursor = db.connection.cursor()
-            sql=""" CALL crear_cita('{}','{}','{}','{}') """.format(cita.fechaCita,cita.horaInicio,cita.horaFin,cita.idUsuarioMedico)
-            cursor.execute(sql)
+
+            # Primero insertamos en la tabla CitasReservadas
+            sql_citas_reservadas = """INSERT INTO CitasReservadas (idUsuarioPaciente, idUsuarioMedico)
+                                      VALUES ('{}', '{}')""".format(cita.idUsuarioPaciente, cita.idUsuarioMedico)
+            cursor.execute(sql_citas_reservadas)
             db.connection.commit()
+
+            # Obtenemos el idCita autoincrementado generado en la tabla CitasReservadas
+            id_cita = cursor.lastrowid
+
+            # Verificamos si hay alguna cita existente con la misma hora y médico
+            sql_verificar_citas = """SELECT COUNT(*) FROM CitaInformacion
+                                     WHERE idUsuarioMedico = '{}' AND fechacita = '{}' AND 
+                                     (horaInicio <= '{}' AND horaFin >= '{}')""".format(
+                cita.idUsuarioMedico, cita.fechaCita, cita.horaFin, cita.horaInicio)
+            cursor.execute(sql_verificar_citas)
+            count = cursor.fetchone()[0]
+
+            if count > 0:
+                # Si hay citas que se superponen, no realizamos la inserción y retornamos False
+                return False
+
+            # Insertamos en la tabla CitaInformacion
+            sql_cita_informacion = """INSERT INTO CitaInformacion (idCita, fechacita, horaInicio, horaFin)
+                                      VALUES ('{}', '{}', '{}', '{}')""".format(
+                id_cita, cita.fechaCita, cita.horaInicio, cita.horaFin)
+            cursor.execute(sql_cita_informacion)
+            db.connection.commit()
+
             return True
         except Exception as ex:
             raise Exception(ex)
+
         
     @classmethod
     def obtenerEspecialidades(self,db):
