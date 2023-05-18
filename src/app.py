15d6ff4f6f7abf490e4ab729eb3d10341b2e1a68
victor11s <vic.manuel35@gmail.com>
2023-05-17 18:44:53 -0600

    ajustes

--- src/app.py
+++ src/app.py
@@ -543,4 +543,4 @@ def logout():
 if __name__ == '__main__':
     #esto me sirve para cargar la configuracion de desarrollo
     app.config.from_object(config['development'])
-    app.run(debug=True, port=8000)
+    app.run(debug=True)
