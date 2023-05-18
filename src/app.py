57358e1da728fae5e4c302f5a6ccbdd2943f2e79
victor11s <vic.manuel35@gmail.com>
2023-05-17 22:49:43 -0600

    ajustes

--- src/app.py
+++ src/app.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 db=MySQL(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     #cuando se hace una entrada normal, te lleva a la pagina principal
     return redirect(url_for('login'))
